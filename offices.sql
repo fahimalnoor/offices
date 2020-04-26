127adbd733a9f7728592b0340c9e1afe25a62785
Fahim Al-Noor <33399593+fahimalnoor@users.noreply.github.com>
2020-04-26 19:26:46 +0600

    updated database

--- offices.sql
+++ offices.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2020 at 10:02 AM
+-- Generation Time: Apr 26, 2020 at 03:24 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- PHP Version: 7.3.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `office`
+-- Database: `offices`
 --
 
 -- --------------------------------------------------------
@@ -40,7 +39,7 @@ CREATE TABLE `branches` (
 --
 
 INSERT INTO `branches` (`bid`, `phone`, `address`, `userid`) VALUES
-(4, '123123', 'btest', 2);
+(7, '123123', 'test address', 18944);
 
 -- --------------------------------------------------------
 
@@ -57,6 +56,13 @@ CREATE TABLE `btransactions` (
   `date` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
   `requested` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `btransactions`
+--
+
+INSERT INTO `btransactions` (`transaction_serial`, `bid`, `bofficename`, `totaltransactions`, `totalamount`, `date`, `requested`) VALUES
+(2, 7, 'Mirpur', 2400, 1000000, '2020-04-25 09:41:05.244125', 0);
 
 -- --------------------------------------------------------
 
@@ -76,7 +82,7 @@ CREATE TABLE `circles` (
 --
 
 INSERT INTO `circles` (`cid`, `phone`, `address`, `userid`) VALUES
-(1, '123123', 'test address', 5);
+(2, '123123', 'test address', 973275);
 
 -- --------------------------------------------------------
 
@@ -93,6 +99,14 @@ CREATE TABLE `ctransactions` (
   `date` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
   `requested` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ctransactions`
+--
+
+INSERT INTO `ctransactions` (`transaction_serial`, `cid`, `cofficename`, `totaltransactions`, `totalamount`, `date`, `requested`) VALUES
+(1, 2, 'Uttara', 500, 3000000, '2020-04-24 11:04:54.295561', 0),
+(2, 2, 'Uttara', 300, 1500000, '2020-04-25 10:21:44.141682', 0);
 
 -- --------------------------------------------------------
 
@@ -112,7 +126,7 @@ CREATE TABLE `dgs` (
 --
 
 INSERT INTO `dgs` (`dgid`, `phone`, `address`, `userid`) VALUES
-(1, '123123', 'dgtest', 7);
+(3, '123123', 'test address', 877026);
 
 -- --------------------------------------------------------
 
@@ -132,7 +146,7 @@ CREATE TABLE `dpmgs` (
 --
 
 INSERT INTO `dpmgs` (`dpid`, `phone`, `address`, `userid`) VALUES
-(1, '123123', 'test address', 8);
+(2, '123123', 'test address', 946916);
 
 -- --------------------------------------------------------
 
@@ -149,6 +163,14 @@ CREATE TABLE `dptransactions` (
   `date` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
   `requested` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dptransactions`
+--
+
+INSERT INTO `dptransactions` (`transaction_serial`, `dpid`, `dpofficename`, `totaltransactions`, `totalamount`, `date`, `requested`) VALUES
+(1, 2, 'Uttara', 60, 4000000, '2020-04-24 11:08:42.868371', 0),
+(2, 2, 'Uttara', 220, 1200000, '2020-04-25 11:17:09.449820', 0);
 
 -- --------------------------------------------------------
 
@@ -168,7 +190,7 @@ CREATE TABLE `heads` (
 --
 
 INSERT INTO `heads` (`hid`, `phone`, `address`, `userid`) VALUES
-(1, '123123', 'test address', 10);
+(3, '123123', 'test address', 512595);
 
 -- --------------------------------------------------------
 
@@ -190,16 +212,37 @@ CREATE TABLE `logs` (
 
 INSERT INTO `logs` (`userid`, `uname`, `name`, `utype`, `pass`) VALUES
 (1, 'adm', 'Super Admin', 'admin', 'a373eec3e32d1801034b71245a0b70cec1878c6a'),
-(2, 'btest', 'btest', 'branch', 'af283c19cc8548a747082ff1d6a24d546399de12'),
-(3, 'buttara', 'Uttara Branch', 'branch', '3537327efe20518347ae5f794cb2e56104cc7255'),
-(4, 'cadm', 'Uttara Circle', 'circle', '76c7d4861055d60fce1d49aec90f5228ccc6961d'),
-(5, 'ctest', 'ctest', 'circle', '014b4d83fe9ef4e5b47da5675a2ba52e71210a2a'),
-(6, 'dgadm', 'DG', 'dg', 'e71b5b7a08d412150e75b08edfbc883703bc6d7d'),
-(7, 'dgtest', 'dgtest', 'dg', '9de434430a468b7196494806cb0b48360d1a3cb8'),
-(8, 'dptest', 'dptest', 'dpgm', 'd642af106e42caa22d659607e6823257e11a45e8'),
-(9, 'dptest2', 'dptest2', 'dpmg', '8ff41d95434fadfddb4f52aca53de7150f548560'),
-(10, 'hadm', 'Head', 'head', '2233e7ff470e4aa89307000c5fd4c4d2ecde215e'),
-(11, 'htest', 'htest', 'head', 'e70227ee1fe22520832279d4d8e220086e8a663a');
+(18944, 'btest', 'Test Branch', 'branch', 'af283c19cc8548a747082ff1d6a24d546399de12'),
+(512595, 'htest', 'Test Head', 'head', 'e70227ee1fe22520832279d4d8e220086e8a663a'),
+(877026, 'dgtest', 'Test DG', 'dg', '9de434430a468b7196494806cb0b48360d1a3cb8'),
+(946916, 'dptest', 'Test DPMG', 'dpmg', 'd642af106e42caa22d659607e6823257e11a45e8'),
+(973275, 'ctest', 'Test Circle', 'circle', '014b4d83fe9ef4e5b47da5675a2ba52e71210a2a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `msgid` int(10) NOT NULL,
+  `sender` varchar(50) NOT NULL,
+  `sender_utype` varchar(10) NOT NULL,
+  `receiver` varchar(50) NOT NULL,
+  `receiver_utype` varchar(10) NOT NULL,
+  `msgbody` text NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`msgid`, `sender`, `sender_utype`, `receiver`, `receiver_utype`, `msgbody`, `date`) VALUES
+(72060706, 'dptest', 'dpmg', 'btest', 'branch', 'Test message to branch office user btest from dpmg office user dptest.    ', '2020-04-26'),
+(191010050, 'htest', 'head', 'btest', 'branch', 'Test message to branch office user btest from head office user htest.', '2020-04-26'),
+(397323911, 'ctest', 'circle', 'htest', 'head', 'From circle admin ctest to Head admin htest test message.', '2020-04-26'),
+(499190141, 'dgtest', 'dg', 'htest', 'head', 'Test message from dg admin dgtest to Head admin htest.\r\nAll the best!\r\nGood Luck!\r\nBye!', '2020-04-26');
 
 --
 -- Indexes for dumped tables
@@ -268,6 +311,12 @@ ALTER TABLE `logs`
   ADD PRIMARY KEY (`userid`);
 
 --
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`msgid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -275,55 +324,55 @@ ALTER TABLE `logs`
 -- AUTO_INCREMENT for table `branches`
 --
 ALTER TABLE `branches`
-  MODIFY `bid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `bid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `btransactions`
 --
 ALTER TABLE `btransactions`
-  MODIFY `transaction_serial` bigint(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_serial` bigint(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `circles`
 --
 ALTER TABLE `circles`
-  MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `ctransactions`
 --
 ALTER TABLE `ctransactions`
-  MODIFY `transaction_serial` bigint(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_serial` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `dgs`
 --
 ALTER TABLE `dgs`
-  MODIFY `dgid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `dgid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `dpmgs`
 --
 ALTER TABLE `dpmgs`
-  MODIFY `dpid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `dpid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `dptransactions`
 --
 ALTER TABLE `dptransactions`
-  MODIFY `transaction_serial` bigint(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_serial` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `heads`
 --
 ALTER TABLE `heads`
-  MODIFY `hid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `hid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `logs`
+-- AUTO_INCREMENT for table `messages`
 --
-ALTER TABLE `logs`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `messages`
+  MODIFY `msgid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=695392883;
 
 --
 -- Constraints for dumped tables
